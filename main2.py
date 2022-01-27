5ae87d6e2d727dc47e6e9020eb80e955dbd0e26d
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-01-27 17:47:38 +0000

    clean code

--- main2.py
+++ main2.py
@@ -2,39 +2,39 @@
 
 from flask import Flask, render_template
 
-DEVELOPMENT_ENV  = True
+DEVELOPMENT_ENV = True
 
 app = Flask(__name__)
 
 app_data = {
-    "name":         "Peter's Starter Template for a Flask Web App",
-    "description":  "A basic Flask app using bootstrap for layout",
-    "author":       "Peter Simeth",
-    "html_title":   "Peter's Starter Template for a Flask Web App",
+    "name": "Peter's Starter Template for a Flask Web App",
+    "description": "A basic Flask app using bootstrap for layout",
+    "author": "Peter Simeth",
+    "html_title": "Peter's Starter Template for a Flask Web App",
     "project_name": "Starter Template",
-    "keywords":     "flask, webapp, template, basic"
+    "keywords": "flask, webapp, template, basic",
 }
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', app_data=app_data)
+    return render_template("index.html", app_data=app_data)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return render_template('about.html', app_data=app_data)
+    return render_template("about.html", app_data=app_data)
 
 
-@app.route('/service')
+@app.route("/service")
 def service():
-    return render_template('service.html', app_data=app_data)
+    return render_template("service.html", app_data=app_data)
 
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
-    return render_template('contact.html', app_data=app_data)
+    return render_template("contact.html", app_data=app_data)
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=DEVELOPMENT_ENV)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=DEVELOPMENT_ENV)
