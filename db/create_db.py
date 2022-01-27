6a138c6ceab968eec8f270dc5c4089ce60e80b87
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-01-27 20:20:44 +0000

    create weight table

--- db/create_db.py
+++ db/create_db.py
@@ -2,3 +2,15 @@ import sqlite3
 import config
 
 # Connect to database
+connection = sqlite3.connect(config.DB_FILE_PATH)
+
+# Create cursor object
+cursor = connection.cursor("""
+    CREATE TABLE IF NOT EXISTS weight (
+        id INTEGER PRIMARY KEY,
+        date TEXT NOT NULL,
+        weight REAL NOT NULL
+    )
+""")
+
+connection.commit()
