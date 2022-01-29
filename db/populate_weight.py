db9edc1c0b99662bebe6eff92f4712ef74680afa
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-01-29 17:10:50 +0000

    add SQLite3 connection

--- db/populate_weight.py
+++ db/populate_weight.py
@@ -2,3 +2,20 @@ import sqlite3
 from sqlite3 import Error
 import config
 import pathlib
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    connection = None
+    try:
+        connection = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return connection
+
+# Connect to Sqlite db
+connection = create_connection(config.DB_FILE_PATH)
