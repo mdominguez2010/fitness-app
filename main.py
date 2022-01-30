e14c41c6788030f7418010d74390695b5a1ee8fb
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-01-30 16:50:35 +0000

    remove data items

--- main.py
+++ main.py
@@ -16,9 +16,8 @@ def index():
 def progress():
 
     items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+        {'id': 1, 'name': 'Weight'},
+        {'id': 2, 'name': 'Strength'},
     ]
 
     return render_template("progress.html", items=items)
