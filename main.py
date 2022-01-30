0e963bcecded44e6b4e6e1e5fe271f9c8bc71f82
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-01-30 17:45:08 +0000

    fingers crossed

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 ###### IN DEVELOPMENT #########
 
 from flask import Flask, render_template
+app = Flask(__name__)
 
 DEVELOPMENT_ENV = True
 
-app = Flask(__name__)
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
-
-@app.route("/")
-@app.route("/home")
-def index():
-    return render_template("index.html")
-    
-@app.route("/progress")
-def progress():
-
+@app.route('/market')
+def market_page():
     items = [
         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
-
-    return render_template("progress.html", items=items)
+    return render_template('market.html', items=items)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=DEVELOPMENT_ENV)
