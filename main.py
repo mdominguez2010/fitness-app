de9b57d69c5f3d9b1b883ed2ba9f1b88e0a7acea
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-01-30 18:33:02 +0000

    going nuclear again

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ DEVELOPMENT_ENV = True
 def home_page():
     return render_template('home.html')
 
-@app.route('/progress')
+@app.route('/market')
 def market_page():
     items = [
-        {'id': 1, 'name': 'Phone'},
-        {'id': 2, 'name': 'Laptop'},
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
-    return render_template('progress.html', items=items)
+    return render_template('market.html', items=items)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=DEVELOPMENT_ENV)
