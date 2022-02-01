3953c66491867a7a4c3d26688d688132c9f1ebe0
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-02-01 23:39:20 +0000

    add weight viz

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template
+import sqlite3
+import db.config
+import plotly.express as px
+
 app = Flask(__name__)
 
 DEVELOPMENT_ENV = True
+
+def executeSQL(db_filepath, sql_query, values=None):
+    """Creates sqlite object and executes an SQL query
+
+    Args:
+        db_filepath (string): absolute filepath for the sqlite3 db
+        sql_query (string): SQL query to execute
+
+    Returns:
+        sqlite3 cursor object
+    """
+    global connection
+
+    connection = sqlite3.connect(db_filepath)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute(f"""{sql_query}""", values)
+    
+    return cursor
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -16,6 +41,28 @@ def progress_page():
         {'id': 3, 'name': 'Cardio'}
     ]
     return render_template('progress.html', items=items)
+
+@app.route('/progress/weight')
+def weight_page():
+
+    QUERY = "SELECT date(date) as date, MIN(weight) as weight FROM weight WHERE date(date) > '2020-12-31' GROUP BY date ORDER BY date ASC;"
+    VALUES = () # simple query, no ETL
+
+    cursor = executeSQL(db.config.DB_FILE_PATH, sql_query=QUERY, values=VALUES)
+    rows = cursor.fetchall()
+
+    X = [row["date"] for row in rows]
+    Y = [row["weight"] for row in rows]
+
+    connection.commit()
+    connection.close()
+
+    fig = px.line(x=X, y=Y)
+    # fig.show()
+
+    data = {'Plot': fig.show()}
+
+    return render_template('weight.html', items=data)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=DEVELOPMENT_ENV)
---
+++ sandbox.py
@@ -0,0 +1,46 @@
+import sqlite3
+import db.config
+import plotly.express as px
+
+def executeSQL(db_filepath, sql_query, values=None):
+    """Creates sqlite object and executes an SQL query
+
+    Args:
+        db_filepath (string): absolute filepath for the sqlite3 db
+        sql_query (string): SQL query to execute
+
+    Returns:
+        sqlite3 cursor object
+    """
+    global connection
+
+    connection = sqlite3.connect(db_filepath)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute(f"""{sql_query}""", values)
+    
+    return cursor
+
+QUERY = "SELECT date(date) as date, MIN(weight) as weight FROM weight WHERE date(date) > '2020-12-31' GROUP BY date ORDER BY date ASC;"
+VALUES = () # simple query, no ETL
+
+cursor = executeSQL(db.config.DB_FILE_PATH, sql_query=QUERY, values=VALUES)
+rows = cursor.fetchall()
+
+# for row in rows:
+#     print(row["date"], row["weight"])
+
+X = [row["date"] for row in rows]
+Y = [row["weight"] for row in rows]
+
+connection.commit()
+connection.close()
+
+# print(X[:5])
+# print(Y[:5])
+
+# print(type(X[0]))
+# print(type(Y[0]))
+
+fig = px.line(x=X, y=Y)
+fig.show()
