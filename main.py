b28c92653f7a866d1f4b262af122f6f37eccb3e7
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-02-01 18:34:47 +0000

    add progress page

--- main.py
+++ main.py
@@ -19,5 +19,14 @@ def market_page():
     ]
     return render_template('market.html', items=items)
 
+@app.route('/progress')
+def progress_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('progress.html', items=items)
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=DEVELOPMENT_ENV)
