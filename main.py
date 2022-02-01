2ef0efe3b27269aafe0c3fd1df060dc9270b62e1
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-02-01 18:43:04 +0000

    come cleanup

--- main.py
+++ main.py
@@ -10,21 +10,12 @@ DEVELOPMENT_ENV = True
 def home_page():
     return render_template('home.html')
 
-# @app.route('/market')
-# def market_page():
-#     items = [
-#         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-#         {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-#         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-#     ]
-#     return render_template('market.html', items=items)
-
 @app.route('/progress')
 def progress_page():
     items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+        {'id': 1, 'name': 'Weight'},
+        {'id': 2, 'name': 'Strength'},
+        {'id': 3, 'name': 'Cardio'}
     ]
     return render_template('progress.html', items=items)
 
