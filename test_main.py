45ee9a7db345d02b29dd1c7cb89472fffd166e5e
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-02-05 09:25:10 -0800

    unit test cont.

--- test_main.py
+++ test_main.py
@@ -9,6 +9,11 @@ class TestMain(unittest.TestCase):
         tester = main.app.test_client(self)
         response = tester.get("/", content_type="html/text")
         self.assertEqual(response.status_code, 200)
+        
+    def weight(self):
+        tester = main.app.test_client(self)
+        response = tester.get("/weight", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
                  
 if __name__ == "__main__":
     unittest.main()       
