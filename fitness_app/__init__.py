8efccd7ad2a5e016c06032a9c1e6a294726a075d
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-02-08 16:59:42 -0800

    fix table header

--- fitness_app/__init__.py
+++ fitness_app/__init__.py
@@ -5,7 +5,6 @@ from fitness_app.db import config
 # import db.config
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + config.DB_FILE_PATH
-# app.config["SERVER_NAME"] = "127.0.0.1:5000"
 app_db = SQLAlchemy(app)
 
 from fitness_app import routes
