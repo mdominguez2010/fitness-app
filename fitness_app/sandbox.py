650b82187c48f6f238e99f3c92b215767d7ae715
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-03-04 21:37:25 +0000

    major cleanup

--- fitness_app/sandbox.py
+++ fitness_app/sandbox.py
@@ -1,9 +1,6 @@
-from fitness_app import app
-from fitness_app.db import config
+import datetime
 import sqlite3
 from sqlite3 import Error
-from flask import render_template
-import datetime
 
 
 
@@ -45,27 +42,29 @@ def get_data():
         
         if tables[i] == "weight":
 
-            print(tables[i])
+            # print(tables[i])
             
-            query = "SELECT date(date) as date, MIN(weight) as weight FROM weight WHERE date(date) > '2020-12-31' GROUP BY date ORDER BY date ASC;"
-            values = ()
+            # query = "SELECT date(date) as date, MIN(weight) as weight FROM weight WHERE date(date) > '2020-12-31' GROUP BY date ORDER BY date ASC;"
+            # values = ()
             
-            connection, cursor = executeSQL(
-                config.DB_FILE_PATH, sql_query=query, values=values
-            )
+            # connection, cursor = executeSQL(
+            #     config.DB_FILE_PATH, sql_query=query, values=values
+            # )
             
-            rows = cursor.fetchall()
+            # rows = cursor.fetchall()
             data_dict[tables[i]] = {
-                "date": [row["date"] for row in rows],
-                "daily_weight": [row["weight"] for row in rows],
+                "date": datetime.datetime.strptime("2022/03/04", "%Y/%m/%d"),
+                "daily_weight": 250,
                 "goal": int(210),
                 "progress": int(-5),
-                "start_date": datetime.datetime.strptime("2022/01/01", "%y/%m/%d")
+                "start_date": datetime.datetime.strptime("2022/01/01", "%Y/%m/%d")
             }
             
         elif tables[i] == "workouts":
             
-            print(tables[i])
+            # print(tables[i])
+
+            query = ""
             
             data_dict[tables[i]] = {
                 "one_rep_max": {
@@ -85,7 +84,9 @@ def get_data():
 
         else:
             
-            print(tables[i])
+            # print(tables[i])
+
+            query = ""
             
             data_dict[tables[i]] = {
                 "runs": {
@@ -96,43 +97,11 @@ def get_data():
                 }
             }
 
-    connection.commit()
-    connection.close()
+    # connection.commit()
+    # connection.close()
     
     print(data_dict)
 
     return data_dict
 
 get_data()
-
-############## Final data object should look like this ##############
-my_dict = {
-    "weight": {
-        "date": [1, 2, 3], 
-        "daily weight": [200, 204, 206], # list of weights
-        "Goal": 200,
-        "Progress": -5,
-        "progress since": "2022-01-01", # start date of goals       
-    },
-    "workouts": {
-        "one_rep_max": {
-            "date": [1, 2, 3], # tracked over time
-            "Deadlift": [1, 2, 3],
-            "Back Squat": [1, 2, 3],
-            "Barbell Bench Press": [1, 2, 3],
-            "Pull Up": [1, 2, 3]
-        },
-        "max_volume": {
-            "Deadlift": 1000,
-            "Back Squat": 2000,
-            "Barbell Bench Press": 3000,
-            "Pull Up": 10
-        }
-    },
-    "runs": {
-        "date": [1, 2, 3],
-        "mile_time": [9, 10, 11],
-        "fastest mile": 9, # minutes
-        "longest run": 5 # miles 
-    }
-}
