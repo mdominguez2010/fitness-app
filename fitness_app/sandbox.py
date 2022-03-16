7c560cf1443595e4f4c5907ad62dfe1a09c6d485
Marcos Dominguez <mdominguez2010@yahoo.com>
2022-03-16 17:32:33 +0000

    adding back-end logic

--- fitness_app/sandbox.py
+++ fitness_app/sandbox.py
@@ -35,7 +35,7 @@ def get_data():
     """
 
     tables = ["weight", "workouts", "runs"]
-    measurements = ["one_rep_max", "max_volume"]
+    measurements = ["exericise_volume", "one_rep_max"]
     exercises = ["Deadlift", "Back Squat", "Barbell Bench Press", "Pull Up"]
     
     data_dict = dict()
@@ -66,33 +66,52 @@ def get_data():
             
             # print(tables[i])
 
-            query = ""
+            
 
             for measurement in measurements:
 
-                    # print(exercise)
-                    # Build one_rep_max data
+                    if measurement == "exercise_volume":
 
-                    for exercise in exercises:
+
+                        for exercise in exercises:
+                            
+                            # Exercise volume
+                            query_exercise_volume = f"SELECT exercise, SUM(reps * weight) as volume FROM workouts WHERE exercise = '{exercise}' GROUP BY date HAVING SUM(reps * weight) > 0 ORDER BY date ASC LIMIT 10;"
+                            connection, cursor = executeSQL(config.DB_FILE_PATH, sql_query=query_exercise_volume, values=None)
+                            rows_exercise_volume = cursor.fetchall()
+
+                            data_dict[tables[i]][measurement]["date"] = [x for x in range(len(rows_exercise_volume))]
+                            data_dict[tables[i]][measurement][exercise] = [rows_exercise_volume["volume"] for row in rows_exercise_volume]
                         
-                        data_dict[tables[i]][measurement]["date"] = datetime.datetime.strptime("2022/03/04", "%Y/%m/%d")
-                        data_dict[tables[i]][measurement][exercise] = 1
-                    
-                        # data_dict[tables[i]] = {
-                        #     "one_rep_max": {
-                        #         "date": [1, 2, 3],
-                        #         "Deadlift": [1, 2, 3],
-                        #         "Back Squat": [1, 2, 3],
-                        #         "Barbell Bench Press": [1, 2, 3],
-                        #         "Pull Up": [1, 2, 3]
-                        #     },
-                        #     "max_volume": {
-                        #         "Deadlift": 1000,
-                        #         "Back Squat": 2000,
-                        #         "Barbell Bench Press": 3000,
-                        #         "Pull Up": 10                    
-                        #     }                    
-                        # }
+                            # data_dict[tables[i]] = {
+                            #     "exercise_volume": {
+                            #         "date": [],
+                            #         "Deadlift": [],
+                            #         "Back Squat": [],
+                            #         "Barbell Bench Press": [],
+                            #         "Pull Up": []
+                            #   },
+                            #     "one_rep_max": {
+                            #         "date": 1,
+                            #         "Deadlift": 200,
+                            #         "Back Squat": 200,
+                            #         "Barbell Bench Press": 200,
+                            #         "Pull Up": 10
+                            #     }              
+                            # }
+                        
+
+                    else:
+
+                        for exercise in exercises:
+                            
+                            # One-rep max
+                            query_one_rep_max = f";"
+                            connection, cursor = executeSQL(config.DB_FILE_PATH, sql_query=query_one_rep_max, values=None)
+                            rows_one_rep_max = cursor.fetchone()
+
+                            data_dict[tables[i]][measurement]["date"] = [x for x in range(len(rows_one_rep_max))]
+                            data_dict[tables[i]][measurement][exercise] = [rows_one_rep_max["volume"] for row in rows_one_rep_max]
 
         else:
             
